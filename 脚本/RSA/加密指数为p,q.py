c01acdae6c8066f71e50e6d753fd7a8d805afc47
dawn1ight <109656216+dawn1ight@users.noreply.github.com>
2023-06-19 23:11:01 +0800

    Update 加密指数为p,q.py

--- 脚本/RSA/加密指数为p,q.py
+++ 脚本/RSA/加密指数为p,q.py
@@ -9,3 +9,16 @@ p = libnum.gcd(pow(x,n)-y,y)
 q = n//p
 d = libnum.invmod(p,(p-1)*(q-1))
 print(libnum.n2s(pow(x,d,n)))
+
+# P = pow(m, p, n)
+# Q = pow(m, q, n)
+# R = pow(m, r, n)
+
+n=
+P=
+Q=
+R=
+PR.<m> = PolynomialRing(Zmod(n))
+f = -m^3+(P+Q+R)*m^2-(P*Q+P*R+R*Q)*m+P*Q*R
+m = f.monic().small_roots(X=2^256)
+print(m)
